25b0485b7db6210fc736bcfd53711b36e1b7acc4
petrakiio <eup0022@gmail.com>
2026-02-02 22:12:46 -0500

    HTML e css da primeira pagina feitos

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from routes.home_routes import home
 
 app = Flask(__name__)
+app.register_blueprint(home)
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5000,debug=True)
--- routes/home_routes.py
+++ routes/home_routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint,render_template,redirect,url_for
 
+home = Blueprint('Home',__name__)
+
+@home.route('/')
+@home.route('/index')
+def index():
+    return render_template('index.html')
