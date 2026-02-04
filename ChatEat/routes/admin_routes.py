b82cd0ee7864b4abf2d4b3fa162110510e66d886
petrakiio <eup0022@gmail.com>
2026-02-04 18:32:46 -0500

    Paginas adicionada

--- ChatEat/routes/admin_routes.py
+++ ChatEat/routes/admin_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template,url_for,redirect,flash,request
 import os
 from models.admin_class import AdminService
 from routes.auth import admin_required
+from models.shopp_class import Product
 
 admin_route = Blueprint('admin', __name__)
 
@@ -18,7 +19,40 @@ def admin_user():
 @admin_route.route('/admin_produtos')
 @admin_required
 def admin_produtos():
-    return render_template('admin_produtos.html',produtos=AdminService.products())
+    return render_template('admin_produtos.html',produtos=Product.get_all_products())
+
+@admin_route.route('/admin_produtos/novo', methods=['GET', 'POST'])
+@admin_required
+def admin_produtos_novo():
+    if request.method == 'POST':
+        flash('Salvar produto ainda não implementado.', 'danger')
+        return redirect(url_for('admin.admin_produtos'))
+    return render_template('admin_produtos_add.html')
+
+@admin_route.route('/admin_produtos/editar/<int:id>', methods=['GET', 'POST'])
+@admin_required
+def admin_produtos_editar(id):
+    if request.method == 'POST':
+        flash('Salvar alterações ainda não implementado.', 'danger')
+        return redirect(url_for('admin.admin_produtos'))
+    produto = Product.get_product_by_id(id)
+    if not produto:
+        flash('Produto não encontrado.', 'danger')
+        return redirect(url_for('admin.admin_produtos'))
+    produto = {
+        'id': produto.get('id'),
+        'nome': produto.get('nome', ''),
+        'preco': produto.get('preco', ''),
+        'descricao': produto.get('descricao', ''),
+        'imagem': produto.get('imagem', produto.get('img', '')),
+    }
+    return render_template('admin_produtos_edit.html', produto=produto)
+
+@admin_route.route('/admin_produtos/excluir/<int:id>')
+@admin_required
+def admin_produtos_excluir(id):
+    
+    return redirect(url_for('admin.admin_produtos'))
 
 @admin_route.route('/deletar_feedback/<int:id>')
 @admin_required
