f74ab468b29fcd1bbe1a6b40ce8487357ad18735
petrakiio <eup0022@gmail.com>
2026-02-06 19:35:24 -0500

    Tentando subir site

--- ValeLer/app.py
+++ ValeLer/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from routes.home_routes import home
 from routes.admin_routes import admin
 try:
@@ -7,7 +8,9 @@ try:
     app.register_blueprint(admin)
 
     if __name__ == '__main__':
-        app.run(host='localhost', port=5000,debug=True)
+        port = int(os.getenv("PORT", 5000))
+        debug = os.getenv("FLASK_DEBUG", "0") == "1"
+        app.run(host='0.0.0.0', port=port, debug=debug)
 except Exception as err:
     print('Erro:',err)
 finally:
