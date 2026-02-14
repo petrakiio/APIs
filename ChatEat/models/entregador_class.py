a71d3fa8d99aa2a892395dee7d2e1f707d53a7c8
petrakiio <eup0022@gmail.com>
2026-02-13 21:03:32 -0500

    Voltando a fazer o sistema de entregador

--- ChatEat/models/entregador_class.py
+++ ChatEat/models/entregador_class.py
@@ -1,1 +1,17 @@
-from 
+from connection.entregador_conn import get_entregador,add_entregador,rm_entregador
+
+class Entregador:
+    def __init__(self,nome,usuario,email,telefone,veiculo,placa,ativo):
+        self.nome = nome
+        self.usuario = usuario
+        self.emai = email
+        self.telefone = telefone
+        self.veiculo = veiculo
+        self.placa = placa
+        self.ativo = ativo
+
+class EntregadoService:
+
+    @staticmethod
+    def visu():
+        return get_entregador()
