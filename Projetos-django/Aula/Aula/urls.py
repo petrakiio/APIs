ceb9386520596ebcff5d124e95922cebf9835f73
petrakiio <eup0022@gmail.com>
2026-02-24 18:29:01 -0500

    Aprendendo sobre templates

--- Projetos-django/Aula/Aula/urls.py
+++ Projetos-django/Aula/Aula/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste/', view.teste),
-    path('',view.index)
+    path('',view.index),
+    path('tarefas/',include('tarefas.urls'))
 ]
--- Projetos-django/Aula/Aula/view.py
+++ Projetos-django/Aula/Aula/view.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def teste(request):
     return HttpResponse('Olá')
 
 def index(request):
-    return HttpResponse('Ola visitante')
+    return render(request,'home/home.html')
