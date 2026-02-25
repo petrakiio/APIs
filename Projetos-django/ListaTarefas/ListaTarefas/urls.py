4f2022742f2ead13d228a9ddbd7a0ce67d578694
petrakiio <eup0022@gmail.com>
2026-02-24 19:09:43 -0500

    Dando inicio ao CRUD

--- Projetos-django/ListaTarefas/ListaTarefas/urls.py
+++ Projetos-django/ListaTarefas/ListaTarefas/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Aula project.
+URL configuration for ListaTarefas project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from . import view
-
-app_name = 'home'
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teste/', view.teste),
-    path('',include('tarefas.urls')),
-    path('tarefas/',view.index,name='index')
 ]
