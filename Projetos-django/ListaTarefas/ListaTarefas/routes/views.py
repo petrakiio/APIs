bd3b350a18fbbf58c78e885852a906139adfc52b
petrakiio <eup0022@gmail.com>
2026-02-24 21:31:10 -0500

    Sistema de enviar funcional

--- Projetos-django/ListaTarefas/ListaTarefas/routes/views.py
+++ Projetos-django/ListaTarefas/ListaTarefas/routes/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from ..models.forms import TarefaForm
+from ..models.tarefa import TarefasModel
+from django.http import HttpRequest
 
 def home(request):
     contexto = {
-        'nome': 'Pedro'
+        'nome': 'Pedro',
+        'tarefas':TarefasModel.objects.all()
     }
     return render(request, 'home.html', contexto)
 
 
-def adicionar_tarefa(request):
+def adicionar_tarefa(request:HttpRequest):
+    if request.method == 'POST':
+        formulario = TarefaForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('index')
+    else:
+        formulario = TarefaForm()
+
     contexto = {
-        "form": TarefaForm()
+        "form": formulario
     }
     return render(request, 'add.html', contexto)
+
