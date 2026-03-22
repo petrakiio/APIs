f49443546ce404b43cbe91f49fbb0bc002f2107f
petrakiio <eup0022@gmail.com>
2026-03-21 19:18:50 -0500

    Css atualizado e model do user feito

---
+++ Projetos-django/Interact/api/models/auth_model.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model):
+    nome = models.CharField(max_length=150)
+    senha = models.CharField(max_length=250)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(max_length=500, blank=True)
+    descricao = models.CharField(max_length=300)
+    codigo_id = models.CharField(max_length=25,unique=True)
+
+    def __str__(self):
+        return super().__str__()
