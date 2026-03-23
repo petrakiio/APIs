8ec3b6c8c99685d556c76f6599f0a02c21bbbed9
petrakiio <eup0022@gmail.com>
2026-03-23 05:50:43 -0500

    pagina de user feita

--- Projetos-django/Interact/api/controller/user/user_controller.py
+++ Projetos-django/Interact/api/controller/user/user_controller.py
@@ -6,4 +6,9 @@ class UserController:
     @staticmethod
     def profile(request, user_id):
         profile = get_object_or_404(User, id=user_id)
-        return render(request, 'user.html', {'profile': profile})
+        current_user_id = request.session.get('user_id')
+        return render(
+            request,
+            'user.html',
+            {'profile': profile, 'current_user_id': current_user_id},
+        )
