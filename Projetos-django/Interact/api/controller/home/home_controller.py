fea8a608f1ebaf8133ff1028ee0f29ef5813422d
petrakiio <eup0022@gmail.com>
2026-03-24 20:49:09 -0500

    Sistema de visualização feito

--- Projetos-django/Interact/api/controller/home/home_controller.py
+++ Projetos-django/Interact/api/controller/home/home_controller.py
@@ -1,21 +1,15 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from models.auth_model import User
 
 class HomeController:
     @staticmethod
     def index(request):
-        User = get_user_model()
         current_user = None
-
-        if request.user.is_authenticated:
-            current_user = request.user
-            users = User.objects.exclude(id=request.user.id).order_by('id')
+        user_id = request.session.get('user_id')
+        if user_id:
+            current_user = User.objects.filter(id=user_id).first()
+            users = User.objects.exclude(id=user_id).order_by('id')
         else:
-            user_id = request.session.get('user_id')
-            if user_id:
-                current_user = User.objects.filter(id=user_id).first()
-                users = User.objects.exclude(id=user_id).order_by('id')
-            else:
-                users = User.objects.all().order_by('id')
+            users = User.objects.all().order_by('id')
 
         return render(request, 'index.html', {'users': users, 'current_user': current_user})
